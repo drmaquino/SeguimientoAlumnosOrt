efdd403f2b23a0039783480d6c10784e40a84bab
Mariano Aquino (e22138) <Mariano.Aquino@cmegroup.com>
2014-11-05 17:59:33 -0300

    agrega algunos metodos de prueba de grabado a SD y a BD

--- src/main/controllers/GruposActivity.java
+++ src/main/controllers/GruposActivity.java
@@ -1,7 +1,5 @@
 package main.controllers;
 
-import com.app.R;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,36 +10,39 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class GruposActivity extends Activity
-{
-    private ListView lvGrupos;
-    private TextView tvCurso;
+import com.app.R;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_grupos);        
-        
-        Bundle bundle = this.getIntent().getExtras();
-        String[] datosDelCurso = bundle.getStringArray("curso");
-        
-        tvCurso = (TextView) findViewById(R.id.curso);
-        lvGrupos = (ListView) findViewById(R.id.listaDeCursos);
-        
-        tvCurso.setText(String.format("%s, %s, %s", datosDelCurso[0], datosDelCurso[1], datosDelCurso[2]));
-        
-        String[] grupos = new String[]{"Grupo 1", "Grupo 2", "Grupo 3", "Grupo 4", "Grupo 5", "Grupo 6", "Grupo 7"};
-        
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, grupos);
-        lvGrupos.setAdapter(adapter);
-        
-        lvGrupos.setOnItemClickListener(new OnItemClickListener()
-        {
-            public void onItemClick(AdapterView parent, View v, int position, long id)
-            {
-                Toast.makeText(getApplicationContext(), "entrando al grupo!", Toast.LENGTH_LONG).show();
-            }
-        });
-    }
+public class GruposActivity extends Activity {
+	private ListView lvGrupos;
+	private TextView tvCurso;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_grupos);
+
+		Bundle bundle = this.getIntent().getExtras();
+		String[] datosDelCurso = bundle.getStringArray("curso");
+
+		tvCurso = (TextView) findViewById(R.id.curso);
+		lvGrupos = (ListView) findViewById(R.id.listaDeCursos);
+
+		tvCurso.setText(String.format("%s, %s, %s", datosDelCurso[0],
+				datosDelCurso[1], datosDelCurso[2]));
+
+		String[] grupos = new String[] { "Grupo 1", "Grupo 2", "Grupo 3",
+				"Grupo 4", "Grupo 5", "Grupo 6", "Grupo 7" };
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, grupos);
+		lvGrupos.setAdapter(adapter);
+
+		lvGrupos.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView parent, View v, int position,
+					long id) {
+				Toast.makeText(getApplicationContext(), "entrando al grupo!",
+						Toast.LENGTH_LONG).show();
+			}
+		});
+	}
 }
--- src/main/controllers/HomeActivity.java
+++ src/main/controllers/HomeActivity.java
@@ -1,55 +1,133 @@
 package main.controllers;
 
-import main.model.Curso;
-
-import com.app.R;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class HomeActivity extends Activity
-{
+import com.app.R;
+
+public class HomeActivity extends Activity {
     private Spinner spAnios;
     private Spinner spCuatrimestres;
     private Spinner spCursos;
+    private TextView tvTitulo;
+
+    private SQLiteDatabase db;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.activity_home);
 
-        spAnios = (Spinner) findViewById(R.id.spAnios);
-        spCuatrimestres = (Spinner) findViewById(R.id.spCuatrimestres);
-        spCursos = (Spinner) findViewById(R.id.spCursos);
+	spAnios = (Spinner) findViewById(R.id.spAnios);
+	spCuatrimestres = (Spinner) findViewById(R.id.spCuatrimestres);
+	spCursos = (Spinner) findViewById(R.id.spCursos);
+	tvTitulo = (TextView) findViewById(R.id.tvTitulo);
 
-        final Button button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener()
-        {
-            public void onClick(View v)
-            {
-                buscar();
-            }
-        });
+	testGrabandoArchivoEnSD();
+	testCargandoArchivoDesdeSD();
+	testCreandoBaseDeDatos();
+	testInsertandoRegistroDePruebaEnDB();
+
+	final Button button = (Button) findViewById(R.id.button);
+	button.setOnClickListener(new View.OnClickListener() {
+	    public void onClick(View v) {
+		buscar();
+	    }
+	});
     }
-    
-    private void buscar()
-    {
-        String anio = spAnios.getSelectedItem().toString();
-        String cuatri = spCuatrimestres.getSelectedItem().toString();
-        String letra = spCursos.getSelectedItem().toString();
-        Intent intent = new Intent(this, GruposActivity.class);
-        
-        //empaqueto y adjunto los datos del curso y se los mando a la prox activity con el intent
-        Bundle bundle = new Bundle();
-        bundle.putStringArray("curso", new String[]{anio, cuatri, letra});
-        intent.putExtras(bundle);
-        
-        startActivity(intent);
+
+    private void testGrabandoArchivoEnSD() {
+
+	// ESTO NECESITA AGREGAR PERMISOS AL MANIFEST!!
+
+	try {
+	    File myFile = new File("/storage/emulated/0/mysdfile.txt");
+	    myFile.createNewFile();
+	    FileOutputStream fOut = new FileOutputStream(myFile);
+	    OutputStreamWriter myOutWriter = new OutputStreamWriter(fOut);
+	    myOutWriter.append("Titulo de Prueba Cargado desde la SD");
+	    myOutWriter.close();
+	    fOut.close();
+	} catch (Exception e) {
+	    Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG)
+		    .show();
+	}
+    }
+
+    private void testCargandoArchivoDesdeSD() {
+	File sdcard = Environment.getExternalStorageDirectory();
+	File file = new File(sdcard, "mysdfile.txt");
+
+	StringBuilder text = new StringBuilder();
+	try {
+	    BufferedReader br = new BufferedReader(new FileReader(file));
+	    String line;
+
+	    while ((line = br.readLine()) != null) {
+		text.append(line);
+		text.append('\n');
+	    }
+	    br.close();
+	} catch (IOException e) {
+	    Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG)
+		    .show();
+	}
+
+	tvTitulo.setText(text);
+    }
+
+    private void testCreandoBaseDeDatos() {
+	db = openOrCreateDatabase("stud.db",
+		SQLiteDatabase.CREATE_IF_NECESSARY, null);
+	String qry = "create table if not exists stud (_id integer primary key autoincrement, nombre text, apellido text);";
+	db.execSQL(qry);
+    }
+
+    private void testInsertandoRegistroDePruebaEnDB() {
+	db = openOrCreateDatabase("stud.db",
+		SQLiteDatabase.CREATE_IF_NECESSARY, null);
+
+	ContentValues record = new ContentValues();
+
+	record.put("nombre", "mariano");
+	record.put("apellido", "aquino");
+	
+	db.insert("stud", null, record);
+
+	db.close();
+
+    }
+
+    private void buscar() {
+	String anio = spAnios.getSelectedItem().toString();
+	String cuatri = spCuatrimestres.getSelectedItem().toString();
+	String letra = spCursos.getSelectedItem().toString();
+	Intent intent = new Intent(this, GruposActivity.class);
+
+	// empaqueto y adjunto los datos del curso y se los mando a la prox
+	// activity con el intent
+	Bundle bundle = new Bundle();
+	bundle.putStringArray("curso", new String[] { anio, cuatri, letra });
+	intent.putExtras(bundle);
+
+	startActivity(intent);
     }
 }
