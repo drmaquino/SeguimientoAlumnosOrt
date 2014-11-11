5f5ddc4f7a0ab840f38a072cff9f98ef4fae1833
mariano <mariano.aquino@gmail.com>
2014-11-11 00:13:01 -0300

    encapsule una clase con los metodos de grabar y leer archivos

--- gen/com/app/R.java
+++ gen/com/app/R.java
@@ -30,14 +30,13 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090007;
-        public static final int button=0x7f090006;
+        public static final int action_settings=0x7f090006;
+        public static final int btnBuscar=0x7f090005;
         public static final int curso=0x7f090000;
         public static final int listaDeCursos=0x7f090001;
-        public static final int spAnios=0x7f090003;
-        public static final int spCuatrimestres=0x7f090004;
-        public static final int spCursos=0x7f090005;
-        public static final int tvTitulo=0x7f090002;
+        public static final int spAnios=0x7f090002;
+        public static final int spCuatrimestres=0x7f090003;
+        public static final int spCursos=0x7f090004;
     }
     public static final class layout {
         public static final int activity_grupos=0x7f030000;
---
+++ src/main/lib/IOHelper.java
@@ -0,0 +1,99 @@
+package main.lib;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import android.content.Context;
+import android.os.Environment;
+import android.widget.Toast;
+
+
+/**
+ * Ejemplo de uso:
+ * 
+ * IOHelper ioh = new IOHelper(this);
+ * File file = ioh.createLogFile("logs", "log");
+ * ioh.saveToFile(file, "linea 1", true);
+ * ioh.saveToFile(file, "linea 2", true);
+ * ioh.saveToFile(file, "linea 3", true);
+ * String texto = ioh.readFile(file);
+ * Toast.makeText(getBaseContext(), texto, Toast.LENGTH_LONG).show();
+ */
+
+
+public class IOHelper
+{
+    private Context _context;
+
+    public IOHelper(Context context)
+    {
+        this._context = context;
+    }
+
+    public File createLogFile(String folderName, String fileName)
+    {
+        File file = null;
+        try
+        {
+            String externalStoragePath = Environment.getExternalStorageDirectory().toString();
+            File mFolder = new File(externalStoragePath + "/" + folderName);
+            if (!mFolder.exists())
+            {
+                mFolder.mkdir();
+            }
+            file = new File(mFolder.getAbsolutePath(), fileName);
+            saveToFile(file, "", false);
+
+        } catch (Exception e)
+        {
+            Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return file;
+    }
+
+    public void saveToFile(File file, CharSequence texto, boolean append)
+    {
+        try
+        {
+            FileOutputStream fos = new FileOutputStream(file, append);
+            OutputStreamWriter myOutWriter = new OutputStreamWriter(fos);
+            myOutWriter.append(texto);
+            myOutWriter.append("\n");
+            myOutWriter.close();
+            fos.close();
+        } catch (Exception e)
+        {
+            Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+    }
+
+    public String readFile(File file)
+    {
+        StringBuilder text = new StringBuilder();
+        try
+        {
+            String line = "";
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            while ((line = br.readLine()) != null)
+            {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+        } catch (IOException e)
+        {
+            Toast.makeText(getBaseContext(), e.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        return text.toString();
+    }
+
+    private Context getBaseContext()
+    {
+        return this._context;
+    }
+}
