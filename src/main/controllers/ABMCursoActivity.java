46ab1f9f07a25ce037cd5b504e100dc4663f3a12
Mariano Aquino (e22138) <Mariano.Aquino@cmegroup.com>
2014-11-14 12:32:47 -0300

    agrego pantalla de abm de cursos con una listview de cursos

---
+++ src/main/controllers/ABMCursoActivity.java
@@ -0,0 +1,54 @@
+package main.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import main.helper.DBHelper;
+import main.model.Curso;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.app.R;
+
+public class ABMCursoActivity extends Activity
+{
+	private ListView lvCursos;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_abm_curso);
+
+		lvCursos = (ListView) findViewById(R.id.listaDeCursos);
+
+		DBHelper dbh = new DBHelper(this);
+
+		/* traigo los cursos */
+		List<Curso> cursosObjs = dbh.getAllCursos();
+
+		List<String> cursos = new ArrayList<String>();
+		for (Curso curso : cursosObjs)
+		{
+			cursos.add(curso.toString());
+		}
+
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cursos);
+		lvCursos.setAdapter(adapter);
+
+		lvCursos.setOnItemClickListener(new OnItemClickListener()
+		{
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+			{
+				Toast.makeText(getApplicationContext(), "entrando al grupo" + position + "!", Toast.LENGTH_LONG).show();
+			}
+		});
+	}
+
+}
